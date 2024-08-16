f1b5b9981ea12674371106ed09ca6351f3a68c16
HYUNHO <jeny95kr@gmail.com>
2024-08-16 18:49:32 -0400

    declare module for deployment

--- backend/dist/src/routes/messages.route.js
+++ backend/dist/src/routes/messages.route.js
@@ -1,13 +1,8 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const protectRoute_js_1 = __importDefault(require("../middleware/protectRoute.js"));
-const messagesController_js_1 = require("../controllers/messagesController.js");
-const messageRoutes = express_1.default.Router();
-messageRoutes.post("/send/:id", protectRoute_js_1.default, messagesController_js_1.sendMessage);
-messageRoutes.get("/conversations", protectRoute_js_1.default, messagesController_js_1.getUsersForSidebar);
-messageRoutes.get("/:id", protectRoute_js_1.default, messagesController_js_1.getMessages);
-exports.default = messageRoutes;
+import express from "express";
+import protectRoute from "../middleware/protectRoute.js";
+import { getMessages, getUsersForSidebar, sendMessage, } from "../controllers/messagesController.js";
+const messageRoutes = express.Router();
+messageRoutes.post("/send/:id", protectRoute, sendMessage);
+messageRoutes.get("/conversations", protectRoute, getUsersForSidebar);
+messageRoutes.get("/:id", protectRoute, getMessages);
+export default messageRoutes;
